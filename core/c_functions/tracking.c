ae7e492427dac6a97c11b68906bf8845399053a2
Hans Jakob Damsgaard <hansjakobdamsgaard@gmail.com>
2023-01-31 14:32:46 +0200

    tracking conversion final

--- core/c_functions/tracking.c
+++ core/c_functions/tracking.c
@@ -28,7 +28,6 @@ void generateReplica(double* time,
                      double* r_remCarrierPhase,
                      complex double* r_replica) 
 {
-
     double temp;
 
     for (size_t i=0; i < samplesRequired; i++)
@@ -68,7 +67,6 @@ void getCorrelator(double* iSignal,
                    double* r_iCorr,
                    double* r_qCorr)
 {
-
     size_t codeIdx = 0;
     
     double start = remCodePhase + correlatorSpacing;
@@ -86,7 +84,121 @@ void getCorrelator(double* iSignal,
     return;
 }
 
+// --------------------------------------------------------------------------------------------------------------------
 
+/*
+@brief
+@param
+@return void.
+*/
+void generateCarrier(complex double* rfData,
+                     complex double* replica,
+                     size_t size,
+                     double* r_iSignal,
+                     double* r_qSignal)
+{
+    complex double temp;
+
+    for (size_t i=0; i < size; i++)
+    {
+        temp = rfData[i] * replica[i];
+
+        r_iSignal[i] = creal(temp);
+        r_qSignal[i] = cimag(temp);
+    }
+
+    return;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+/*
+@brief
+@param
+@return void.
+*/
+void delayLockLoop(double iEarly,
+                   double qEarly,
+                   double iLate,
+                   double qLate,
+                   double dllTau1,
+                   double dllTau2,
+                   double pdiCode,
+                   double codeNCO,
+                   double codeError,
+                   double codeFrequency,
+                   double* r_codeNCO,
+                   double* r_codeError,
+                   double* r_codeFrequency)
+{
+    double newCodeError = (sqrt(pow(iEarly, 2) + pow(qEarly, 2)) -
+                           sqrt(pow(iLate,  2) + pow(qLate,  2))) /
+                          (sqrt(pow(iEarly, 2) + pow(qEarly, 2)) +
+                           sqrt(pow(iLate,  2) + pow(qLate,  2)));
+
+    *r_codeNCO  = codeNCO;
+    *r_codeNCO += dllTau2 / dllTau1 * (newCodeError - codeError);
+    *r_codeNCO += pdiCode / dllTau1 *  newCodeError;
+
+    *r_codeError = newCodeError;
+
+    *r_codeFrequency = codeFrequency - *r_codeNCO;
+
+    return;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+/*
+@brief
+@param
+@return void.
+*/
+void phaseLockLoop(double iPrompt,
+                   double qPrompt,
+                   double pllTau1,
+                   double pllTau2,
+                   double pdiCarrier,
+                   double carrierNCO,
+                   double carrierError,
+                   double initialFrequency,
+                   double* r_carrierNCO,
+                   double* r_carrierError,
+                   double* r_carrierFrequency)
+{
+    double newCarrierError = atan(qPrompt / iPrompt) / 2.0 / PI;
+
+    *r_carrierNCO  = carrierNCO;
+    *r_carrierNCO += pllTau2 / pllTau1 * (newCarrierError - carrierError);
+    *r_carrierNCO += pdiCarrier / pllTau1 * newCarrierError;
+
+    *r_carrierError = carrierError;
+
+    *r_carrierFrequency = initialFrequency + *r_carrierNCO;
+
+    return;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+/*
+@brief
+@param
+@return void.
+*/
+void getLoopCoefficients(double loopNoiseBandwidth,
+                         double dumpingRatio,
+                         double loopGain,
+                         double* r_tau1,
+                         double* r_tau2)
+{
+    double wn = loopNoiseBandwidth * 8.0 * dumpingRatio / (4.0 * pow(dumpingRatio, 2) + 1);
+
+    *r_tau1 = loopGain / pow(wn, 2);
+    *r_tau2 = 2.0 * dumpingRatio / wn;
+
+    return;
+}
 
 /// ===================================================================================================================
 
--- core/tracking/tracking_epl_c.py
+++ core/tracking/tracking_epl_c.py
@@ -39,8 +39,8 @@ class Tracking(TrackingEPL):
                                         ctypes.c_double,
                                         ctypes.c_double,
                                         ctypes.c_double,
-                                        ndpointer(ctypes.c_double, ndim=1, flags='C_CONTIGUOUS'),
-                                        ndpointer(ctypes.c_double, ndim=1, flags='C_CONTIGUOUS')]
+                                        ndpointer(ctypes.c_double),
+                                        ndpointer(ctypes.c_double)]
         self._getCorrelator.restype = None
 
         self._generateReplica = _lib.generateReplica
@@ -51,32 +51,139 @@ class Tracking(TrackingEPL):
                                           ndpointer(ctypes.cdouble, ndim=1, flags='C_CONTIGUOUS')]
         self._generateReplica.restype = None
 
-        # RUN
+        self._generateCarrier = _lib.generateCarrier
+        self._generateCarrier.argtypes = [ndpointer(ctypes.cdouble, ndim=1, flags='C_CONTIGUOUS'),
+                                          ndpointer(ctypes.cdouble, ndim=1, flags='C_CONTIGUOUS'),
+                                          ctypes.c_size_t,
+                                          ndpointer(ctypes.c_double, ndim=1, flags='C_CONTIGUOUS'),
+                                          ndpointer(ctypes.c_double, ndim=1, flags='C_CONTIGUOUS')]
+        
+        self._delayLockLoop = _lib.delayLockLoop
+        self._delayLockLoop.argtypes = [ctypes.c_double,
+                                        ctypes.c_double,
+                                        ctypes.c_double,
+                                        ctypes.c_double,
+                                        ctypes.c_double,
+                                        ctypes.c_double,
+                                        ctypes.c_double,
+                                        ctypes.c_double,
+                                        ctypes.c_double,
+                                        ctypes.c_double,
+                                        ndpointer(ctypes.c_double),
+                                        ndpointer(ctypes.c_double),
+                                        ndpointer(ctypes.c_double)]
+        self._delayLockLoop.restype = None
+
+        self._phaseLockLoop = _lib.phaseLockLoop
+        self._phaseLockLoop.argtypes = [ctypes.c_double,
+                                        ctypes.c_double,
+                                        ctypes.c_double,
+                                        ctypes.c_double,
+                                        ctypes.c_double,
+                                        ctypes.c_double,
+                                        ctypes.c_double,
+                                        ctypes.c_double,
+                                        ndpointer(ctypes.c_double),
+                                        ndpointer(ctypes.c_double),
+                                        ndpointer(ctypes.c_double)]
+        self._phaseLockLoop.restype = None
+
+        self._getLoopCoefficients = _lib.getLoopCoefficients
+        self._getLoopCoefficients.argtypes = [ctypes.c_double,
+                                              ctypes.c_double,
+                                              ctypes.c_double,
+                                              ndpointer(ctypes.c_double),
+                                              ndpointer(ctypes.c_double)]
+        self._getLoopCoefficients.restype = None
 
     # -------------------------------------------------------------------------
     # METHODS
 
     def getCorrelator(self, correlatorSpacing):
         iCorr, qCorr = np.empty((1,)), np.empty((1,))
-        self._getCorrelator(self.iSignal, self.qSignal, self.code, len(self.code), \
-                            self.codePhaseStep, self.remCodePhase, correlatorSpacing, \
-                            iCorr, qCorr)
+        self._getCorrelator(np.ascontiguousarray(self.iSignal), np.ascontiguousarray(self.qSignal),\
+                            np.ascontiguousarray(self.code), len(self.code), self.codePhaseStep,\
+                            self.remCodePhase, correlatorSpacing, iCorr, qCorr)
         return iCorr[0], qCorr[0]
 
     # -------------------------------------------------------------------------
 
     def generateReplica(self):
         remCarrierPhase, replica = np.empty((1,)), np.empty((self.samplesRequired,), dtype='complex128')
-        self._generateReplica(self.time[:self.samplesRequired+1], self.samplesRequired,\
-                              self.carrierFrequency, self.remCarrierPhase, \
-                              remCarrierPhase, replica)
+        self._generateReplica(np.ascontiguousarray(self.time[:self.samplesRequired+1]), self.samplesRequired,\
+                              self.carrierFrequency, self.remCarrierPhase, remCarrierPhase,\
+                              np.ascontiguousarray(replica))
         self.remCarrierPhase = remCarrierPhase[0]
         return replica
 
     # -------------------------------------------------------------------------
 
     def run(self, rfData):
+        replica = self.generateReplica()
+        iSignal, qSignal = np.empty((len(replica),)), np.empty((len(replica),))
+        self._generateCarrier(np.ascontiguousarray(rfData), np.ascontiguousarray(replica), len(rfData),\
+                              np.ascontiguousarray(iSignal), np.ascontiguousarray(qSignal))
+        self.iSignal = iSignal
+        self.qSignal = qSignal
+
+        # Build correlators (Early-Prompt-Late)
+        iEarly , qEarly  = self.getCorrelator(self.correlatorSpacing[0])
+        iPrompt, qPrompt = self.getCorrelator(self.correlatorSpacing[1])
+        iLate  , qLate   = self.getCorrelator(self.correlatorSpacing[2])
+
+        self.correlatorResults = [iEarly, qEarly, iPrompt, qPrompt, iLate, qLate]
+        
+        # Delay Lock Loop (DLL)
+        self.delayLockLoop(iEarly, qEarly, iLate, qLate)
+        
+        # Phase Lock Loop (PLL)
+        self.phaseLockLoop(iPrompt, qPrompt)
+
+        # Get remaining phase
+        idx = np.linspace(self.remCodePhase, self.samplesRequired * self.codePhaseStep + self.remCodePhase, \
+                          self.samplesRequired, endpoint=False)
+        self.remCodePhase = idx[self.samplesRequired-1] + self.codePhaseStep - self.gnssSignal.codeBits
+
+        self.codePhaseStep = self.codeFrequency / self.rfSignal.samplingFrequency
+        self.samplesRequired = int(np.ceil((self.gnssSignal.codeBits - self.remCodePhase) / self.codePhaseStep))
+        
+        # it will create a log of lines in the logfile if uncommented
+        #logging.getLogger(__name__).debug(f"svid={self.svid}, iprompt={iPrompt: 10.2f}, qprompt={iPrompt: 10.2f}, DLL={self.dll: 5.3f}, PLL={self.dll: 5.3f}")
+
+        return
+
+    # -------------------------------------------------------------------------
+
+    def delayLockLoop(self, iEarly, qEarly, iLate, qLate):
+        codeNCO, codeError, codeFrequency = np.empty((1,)), np.empty((1,)), np.empty((1,))
+        self._delayLockLoop(iEarly, qEarly, iLate, qLate, self.dllTau1, self.dllTau2,\
+                            self.pdiCode, self.codeNCO, self.codeError, self.gnssSignal.codeFrequency,\
+                            codeNCO, codeError, codeFrequency)
+        self.codeNCO = codeNCO[0]
+        self.codeError = codeError[0]
+        self.codeFrequency = codeFrequency[0]
+        self.dll = self.codeNCO
+        return
+
+    # -------------------------------------------------------------------------
+
+    def phaseLockLoop(self, iPrompt, qPrompt):
+        carrierNCO, carrierError, carrierFrequency = np.empty((1,)), np.empty((1,)), np.empty((1,))
+        self._phaseLockLoop(iPrompt, qPrompt, self.pllTau1, self.pllTau2, self.pdiCarrier,\
+                            self.carrierNCO, self.carrierError, self.initialFrequency,\
+                            carrierNCO, carrierError, carrierFrequency)
+        self.carrierNCO = carrierNCO[0]
+        self.carrierError = carrierError[0]
+        self.carrierFrequency = carrierFrequency[0]
+        self.pll = self.carrierNCO
         pass
+
+    # -------------------------------------------------------------------------
+
+    def getLoopCoefficients(self, loopNoiseBandwidth, dumpingRatio, loopGain):
+        tau1, tau2 = np.empty((1,)), np.empty((1,))
+        self._getLoopCoefficients(loopNoiseBandwidth, dumpingRatio, loopGain, tau1, tau2)
+        return tau1[0], tau2[0]
 
     # -------------------------------------------------------------------------
 
