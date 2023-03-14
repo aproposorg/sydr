f8a6b24b7fbe36fe081dbebbd0c74957a8f50e6c
Antoine Grenier <antoine.grenier@laposte.net>
2023-03-14 16:44:38 +0200

    Add ChannelManager class #33 ChannelManager class created, need to be integrated within a new receiver object.

--- core/channel/channelManager.py
+++ core/channel/channelManager.py
@@ -3,11 +3,13 @@ import numpy as np
 import logging
 import multiprocessing
 from multiprocessing import shared_memory
+from queue import Empty
 
 from core.channel.channel import Channel
 from core.signal.gnsssignal import GNSSSignal
 from core.signal.rfsignal import RFSignal
 from core.utils.circularbuffer import CircularBuffer
+
 
 class ChannelManager():
 
@@ -20,7 +22,6 @@ class ChannelManager():
 
     # Communication
     resultQueue : multiprocessing.Queue()
-    timeout : int
 
     def __init__(self, buffersize:int, dtype:np.dtype) -> None:
         """
@@ -36,7 +37,6 @@ class ChannelManager():
         
         Raises:
             None
-
         """
 
         self.channels = []
@@ -45,7 +45,7 @@ class ChannelManager():
         # Allocate shared memory 
         nbytes = buffersize * np.dtype(dtype).itemsize
         self._sharedMemory = shared_memory.SharedMemory(create=True, size=nbytes)
-        self.sharedBuffer = np.ndarray(shape=(1, buffersize), dtype=dtype, buffer=self._sharedMemory.buf)
+        self.sharedBuffer = CircularBuffer(buffersize, dtype, self._sharedMemory)
 
         # RF queue
         self.resultQueue = multiprocessing.Queue()
@@ -54,40 +54,63 @@ class ChannelManager():
 
     # -----------------------------------------------------------------------------------------------------------------
 
-class ChannelManager():
+    def addChannel(self, ChannelObject:type[Channel], configFilePath:str, nbChannels=1):
+        """
+        Add channel(s) to the ChannelManager, providing the Channel type and the number of channels of that type to be
+        added.
 
-    def __init__(self, size:int, dtype:np.int8, timeout:int) -> None:
+        Args:
+            ChannelObject (type[Channel]): Channel class used to create the channel.
+            configFilePath (str): Configuration file of the channel.
+            nbChannels (int): Number of channel of that type to be created.
 
-        self.channels = []
-        self.nbChannels = 0
-
-        # Allocate shared memory 
-        nbytes = size * np.dtype(dtype).itemsize
-        self._sharedMemory = shared_memory.SharedMemory(create=True, size=nbytes)
-        self.sharedBuffer = np.ndarray(shape=(1, size), dtype=dtype, buffer=self._sharedMemory.buf)
-
-        # RF queue
-        self.rfQueue = multiprocessing.Queue()
-        self.receiverPipe = multiprocessing.Pipe()
-        self.resultQueue = multiprocessing.Queue()
-        self.timeout = timeout
-
-        return
-    
-    def addChannel(self, ChannelObject, nbChannels=1):
+        Returns:
+            None
+        
+        Raises:
+            None
+        """
 
         for i in range(nbChannels):
-            events = (multiprocessing.Event(), multiprocessing.Event())
-            self.channels.append(ChannelObject(self.nbChannels, self.sharedBuffer, self.resultQueue, events))
+            self.channels.append(ChannelObject(self.nbChannels, self.sharedBuffer, self.resultQueue))
             self.channels[-1].start()
             self.nbChannels += 1
+        
         return
     
+    # -----------------------------------------------------------------------------------------------------------------
+
     def addNewRFData(self, data):
-        self.sharedBuffer[:] = data
+        """
+        Add new RF signal data in the shared buffer area. 
+
+        Args:
+            data (np.array): New data added to the buffer.
+
+        Returns:
+            None
+        
+        Raises:
+            None
+        """
+        self.sharedBuffer.shift(data)
         return
     
+    # -----------------------------------------------------------------------------------------------------------------
+
     def run(self):
+        """
+        Run the channels based on the current RF data in the buffer. 
+
+        Args:
+            None
+
+        Returns:
+            results (list): Processing results from the channels.
+        
+        Raises:
+            None
+        """
         # Start the channels
         for chan in self.channels:
             chan.event_run.set()
@@ -98,18 +121,26 @@ class ChannelManager():
 
         # Process results
         results = []
-        # qsize is highlighted as "approximate count" in documentation, but in our case we have an event trigger before.
+        # qsize is highlighted as "approximate count" in documentation, but in our case we have an event trigger 
+        # before so the count should be exact.
         while self.resultQueue.qsize() > 0:
             try:
-                packet = self.resultQueue.get(timeout=1)
+                packet = self.resultQueue.get(timeout=self.timeout)
             except Empty:
                 break
-            print(f"From manager: CID {packet['cid']}, {packet['result']}")
             results.append(packet)
                 
         return results
     
+    # -----------------------------------------------------------------------------------------------------------------
+    
     def close(self):
+        """
+        Close the ChannelManager and free the memory allocations made for the buffer. Should be called once at the end
+        of the scenario when exiting the program.
+        """
         self._sharedMemory.close()
         self._sharedMemory.unlink()
         return
+    
+    # -----------------------------------------------------------------------------------------------------------------
