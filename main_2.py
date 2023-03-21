1fa8c76ed75075d081cc532764c1bb5a9ab2d439
Antoine Grenier <antoine.grenier@tuni.fi>
2023-03-21 19:09:27 +0200

    Debug new code structure #40 Added report maker

--- main_2.py
+++ main_2.py
@@ -3,6 +3,7 @@ import configparser
 
 from core.enlightengui import EnlightenGUI
 from core.receiver.receiver_gps_l1ca_mp import ReceiverGPSL1CA
+from core.analysis.visualisationV2 import VisualisationV2
 
 import core.logger as logger
 
@@ -12,8 +13,9 @@ def main():
     """
 
     # Configuration
+    receiverConfigFile = './config/receiver.ini'
     receiverConfig  = configparser.ConfigParser()
-    receiverConfig.read('./config/receiver.ini')
+    receiverConfig.read(receiverConfigFile)
 
     # Create GUI
     gui = EnlightenGUI()
@@ -29,7 +31,15 @@ def main():
     receiver.run()
 
     # Create report
-    # TODO
+    benchmarkConfigFile = './config/benchmark.ini'
+    visual = VisualisationV2(benchmarkConfigFile, receiver.rfSignal)
+    visual.setDatabase(receiver.database)
+    visual.setConfig(receiverConfigFile)
+    gui.updateMainStatus(stage='Create report', status='RUNNING')
+    visual.run()
+
+    # Closing receiver to free database and memory
+    receiver.close()
 
     gui.updateMainStatus(stage=f'PROCESSING COMPLETED', status='DONE')
 
